fedd4aa351e424aaa50ecf2a963d5872a19bf517
matthewhiggins415 <mhiggins.louis@gmail.com>
2023-01-04 11:15:31 -0700

    added alt parameter for image property.

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,7 +4,7 @@ import { ProductContainer, Image } from '../styles/components/Product.styles'
 const Product = ({ data }) => {
   return (
     <ProductContainer to={`/product/${data._id}`}>
-      <Image src={data.imageOne}/>
+      <Image src={data.imageOne} alt={"image of product " + data.name}/>
       <h1>{data.name}</h1>
       <h3>{"$" + `${data.price}`}</h3>
     </ProductContainer>
