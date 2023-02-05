af28798fbe0604eef24b7378e87dde3650674c5d
matthewhiggins415 <mhiggins.louis@gmail.com>
2023-02-04 20:07:24 -0700

    updated styling to product screen.

--- src/styles/screens/ProductScreen.styles.js
+++ src/styles/screens/ProductScreen.styles.js
@@ -13,16 +13,20 @@ export const BackBtn = styled(Link)`
   width: 80px;
   text-align: center;
   text-decoration: none;
-  color: black;
+  color:  #FF5733;
   padding: 10px;
-  border: 1px solid black;
   letter-spacing: 1.2px;
   transition: 150ms ease-in-out;
   margin-bottom: 20px;
+  border-radius: 10px;
 
   :hover {
     color: white;
-    background-color: black;
+    background-color:  #FF5733;
+  }
+
+  @media screen and (min-width: 500px) {  
+    margin-left: 100px;
   }
 `
 
@@ -35,6 +39,11 @@ export const ProductInfoContainer = styled.div`
 export const LargeImg = styled.img`
   width: 300px;
   height: 300px;
+
+  @media screen and (min-width: 500px) {  
+    width: 500px;
+    height: 400px;
+  }
 `
 
 export const SmallImgContainer = styled.div`
@@ -42,12 +51,22 @@ export const SmallImgContainer = styled.div`
   display: flex;
   justify-content: space-between;
   margin-top: 20px;
+
+  @media screen and (min-width: 500px) {  
+    width: 450px;
+  }
 `
 
 export const SmallImg = styled.img`
   width: 80px;
   height: 80px;
   cursor: pointer;
+
+  @media screen and (min-width: 500px) {  
+    width: 100px;
+    height: 100px;
+
+  }
 `
 
 export const Name = styled.h1`
@@ -58,6 +77,11 @@ export const Name = styled.h1`
 export const Description = styled.div`
   width: 300px;
   margin-top: 20px;
+
+  @media screen and (min-width: 500px) {  
+    width: 450px;
+    text-align: center;
+  }
 `
 
 export const Price = styled.h2`
@@ -76,4 +100,8 @@ export const AddToCart = styled.button`
   font-weight: 400;
   letter-spacing: 1.2px;
   color: black;
+
+  @media screen and (min-width: 500px) {  
+    width: 450px;
+  }
 `
